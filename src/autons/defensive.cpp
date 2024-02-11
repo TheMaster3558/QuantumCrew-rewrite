06c6b90de0c56b2d48d1674e11ce9847c41662aa
chawkk6404 <chawk_k@outlook.com>
2024-02-10 17:30:09 -0800

    Refactor defensive autons

--- src/autons/defensive.cpp
+++ src/autons/defensive.cpp
@@ -40,7 +40,7 @@ void Autons::Autons::defensiveQual() {
 
     chassis.setPose(55, 34, 0);
 
-    chassis.follow(defensive_qual_move_from_goal_to_bar_txt, 10, 8000);
+    chassis.moveToPose(5, 58, 270, 7000);
     chassis.waitUntilDone();
 }
 
@@ -54,27 +54,37 @@ void Autons::Autons::defensiveElims() {
     chassis.turnTo(45, 10, 1000);
     chassis.waitUntilDone();
 
-    Actions::Flaps::setFlaps(BACK, makeFlapState(true, true));
+    Actions::Intake::outtake();
+    pros::delay(1000);
+    Actions::Intake::brake();
+
+    chassis.moveToPoint(30, 10, 2000);
+    chassis.waitUntilDone();
+
+    chassis.turnTo(0, 10, 1000);
+    chassis.waitUntilDone();
+
+    Actions::Flaps::setFlaps(FRONT, makeFlapState(true, true));
 
     chassis.moveToPose(5, 10, 90, 2000, {
+        .minSpeed = 110
+    });
+    chassis.waitUntilDone();
+
+    Actions::Flaps::setFlaps(FRONT, makeFlapState(false, false));
+    Actions::Flaps::setFlaps(BACK, makeFlapState(true, true));
+
+    chassis.moveToPose(45, 10, 270, 2000, {
         .forwards = false,
         .minSpeed = 110
     });
     chassis.waitUntilDone();
 
-    Actions::Flaps::setFlaps(BACK, makeFlapState(false, false));
+    chassis.setPose(45, 10, 270);
 
-    chassis.moveToPose(45, 10, 90, 2000, {
-        .minSpeed = 110
-    });
+    chassis.moveToPose(33, 45, 0, 3000);
     chassis.waitUntilDone();
 
-    chassis.moveToPoint(35, 10, 2000, false);
-    chassis.waitUntilDone();
-
-    chassis.turnTo(35, 20, 1000);
-    chassis.waitUntilDone();
-
-    chassis.follow(defensive_elims_move_from_goal_to_bar_txt, 10, 7000);
+    chassis.follow(defensive_elims_move_from_goal_to_bar_txt, 13, 7000);
     chassis.waitUntilDone();
 }
